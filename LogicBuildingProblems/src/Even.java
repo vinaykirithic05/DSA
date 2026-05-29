afac13c94b52987c5db29821775235384f52af57
vinaykirithic05 <vinaykirithic05@gmail.com>
2026-05-29 15:34:41 +0530

    Even Program Completed

--- LogicBuildingProblems/src/Even.java
+++ LogicBuildingProblems/src/Even.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Even {
+//     Using Remainder Method
+    public static boolean isEven(int n){
+         int rem = n % 2 ;
+        if(rem == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+//    Using Bitwise Operator
+    public  static boolean isOdd(int n){
+        if((n & 1) == 0){
+            return true;
+        }
+        else
+            return false;
+    }
+//    Main Function
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number to Check Odd or Even : ");
+        int num = input.nextInt();
+        System.out.println("By Finding the Remainder method  and Bitwise Operator method   ");
+        if(isEven(num) == true && isOdd(num) == true){
+            System.out.println("The number is Even");
+        }
+        else {
+            System.out.println("The number is Odd");
+        }
+    }
 }
